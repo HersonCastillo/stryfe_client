93b84d8bb2516529a553ef2dae83bbb420a32f9f
HersonCastillo <30848872+HersonCastillo@users.noreply.github.com>
2018-09-06 06:42:10 -0600

    example create

---
+++ src/app/admin/cpanel/cpanel.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-cpanel',
+    templateUrl: './cpanel.component.html',
+    styleUrls: ['./cpanel.component.css']
+})
+export class CpanelAdminComponent implements OnInit {
+    constructor(){}
+    ngOnInit(){}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { LoginComponent } from './login/login.component';
 import { RegistrarComponent } from './registrar/registrar.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { CpanelAdminComponent } from './admin/cpanel/cpanel.component';
+import { CategoriasComponent } from './admin/views/categorias/categorias.component';
 
 const app: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registrar', component: RegistrarComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'admin', component: CpanelAdminComponent, children: [
+        { path: 'categorias', component: CategoriasComponent },
+        { path: '', redirectTo: 'categorias', pathMatch: 'full' }
+    ], canActivate: [] },
     { path: 'error', component: ErrorComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'error', pathMatch: 'full' }
@@ -22,7 +28,9 @@ const app: Routes = [
         LoginComponent,
         RegistrarComponent,
         HomeComponent,
-        ErrorComponent
+        ErrorComponent,
+        CpanelAdminComponent,
+        CategoriasComponent
     ],
     imports: [
         BrowserModule,
