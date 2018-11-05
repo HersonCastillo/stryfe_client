ba1c8c6806e404e10db0835179c6d641e2f0ad30
hersoncastillo <hersoncastillo12@hotmail.com>
2018-11-05 08:18:19 -0600

    mini-changes

--- src/app/client-access/users/views/lista-deseos/lista-deseos.component.ts
+++ src/app/client-access/users/views/lista-deseos/lista-deseos.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Producto } from '../../../../interfaces/producto';
+import { Includes } from '../../../../utils/Includes';
 @Component({
-  selector: 'app-lista-deseos',
-  templateUrl: './lista-deseos.component.html',
-  styleUrls: ['./lista-deseos.component.css']
+    selector: 'app-lista-deseos',
+    templateUrl: './lista-deseos.component.html',
+    styleUrls: ['./lista-deseos.component.css']
 })
 export class ListaDeseosComponent implements OnInit {
+    constructor(
+        private includes: Includes
+    ){}
+    public productos: Producto[];
+    ngOnInit(){
+        let lista = sessionStorage.getItem(Includes.NAMES.LISTA_DESEOS);
+        if(lista && lista != null){
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+        } else this.productos = [];
+    }
+    
 }
