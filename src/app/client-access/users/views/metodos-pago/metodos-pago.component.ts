4cce2c6991c590b591374efc290752531ee68916
hersoncastillo <hersoncastillo12@hotmail.com>
2018-11-06 08:56:42 -0600

    menu fix

--- src/app/client-access/users/views/metodos-pago/metodos-pago.component.ts
+++ src/app/client-access/users/views/metodos-pago/metodos-pago.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-metodos-pago',
-  templateUrl: './metodos-pago.component.html',
-  styleUrls: ['./metodos-pago.component.css']
+    selector: 'app-metodos-pago',
+    templateUrl: './metodos-pago.component.html',
+    styleUrls: ['./metodos-pago.component.css']
 })
 export class MetodosPagoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(){}
+    ngOnInit(){}
 }
