b8adbd7914d4f7c5b8cf1748af256bc6dd4004d0
hersoncastillo <hersoncastillo12@hotmail.com>
2018-11-06 11:41:32 -0600

    changes

--- src/app/services/orden.service.ts
+++ src/app/services/orden.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { CRUD, Orden, Respuesta } from '../interfaces/ifs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
-export class OrdenService {
-    constructor() { }
+export class OrdenService implements CRUD<Orden> {
+    constructor(
+        private http: HttpClient
+    ){}
+    crear(value: Orden): Observable<Respuesta> {
+        throw new Error("Method not implemented.");
+    }
+    modificar(value: Orden): Observable<Respuesta> {
+        throw new Error("Method not implemented.");
+    }
+    eliminar(value: Orden): Observable<Respuesta> {
+        throw new Error("Method not implemented.");
+    }
+    listar(reset: boolean): Observable<Orden[]> {
+        throw new Error("Method not implemented.");
+    }
+    obtener(value: Orden): Observable<Orden> {
+        throw new Error("Method not implemented.");
+    }
 }
--- src/app/services/reporte.service.ts
+++ src/app/services/reporte.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { CRUD, Reporte, Respuesta } from '../interfaces/ifs';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ReporteService {
-    constructor() { }
+export class ReporteService implements CRUD<Reporte> {
+    constructor(
+        private http: HttpClient
+    ){}
+    crear(value: Reporte): Observable<Respuesta> {
+        throw new Error("Method not implemented.");
+    }
+    modificar(value: Reporte): Observable<Respuesta> {
+        throw new Error("Method not implemented.");
+    }
+    eliminar(value: Reporte): Observable<Respuesta> {
+        throw new Error("Method not implemented.");
+    }
+    listar(reset: boolean): Observable<Reporte[]> {
+        throw new Error("Method not implemented.");
+    }
+    obtener(value: Reporte): Observable<Reporte> {
+        throw new Error("Method not implemented.");
+    }
 }
