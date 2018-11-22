45891e46862f4f89f697db7bc2a59d1fcbe66e23
Herson <hersoncastillo12@hotmail.com>
2018-11-22 09:01:17 -0600

    change

--- src/app/client-access/buscar/buscar.component.ts
+++ src/app/client-access/buscar/buscar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../interfaces/producto';
 @Component({
     selector: 'app-buscar',
     templateUrl: './buscar.component.html',
@@ -10,15 +11,51 @@ export class BuscarComponent implements OnInit {
 
     constructor(
         private route: ActivatedRoute,
-        private productoProvider: ProductoService
+        private productoProvider: ProductoService,
+        private router: Router
     ){}
+    public productos: Producto[] = [];
+    public filter = {
+        min: 0,
+        max: 10000,
+        input: "",
+        dateAfter: Date
+    }
     ngOnInit(){
         this.route.params.subscribe(p => {
             let prod = p['producto'];
+            this.filter.input = prod;
             this.productoProvider.buscar(prod).subscribe(r => {
-                console.log(r)
+                this.productos = r;
             });
         });
     }
-
+    searchBox(): void{
+        if(this.filter.input.trim().length > 0) this.router.navigate(['/buscar', this.filter.input.trim()]);
+        else this.router.navigate(['/home']);
+    }
+    reduce(str: string): string {
+        try {
+            if (str && str.length <= 10) return str;
+            else return str.slice(0, 7).concat("...");
+        } catch (ex) {
+            return "";
+        }
+    }
+    getImage(imageName: string): any {
+        return this.productoProvider.mostrarImagen(imageName);
+    }
+    changeMin(e: any): void{
+        console.log(e)
+    }
+    changeMax(e: any): void{
+        console.log(e)
+    }
+    isAllow(prod: Producto): boolean {
+        try{
+            return prod.precio <= this.filter.max && prod.precio >= this.filter.min;
+        }catch(ex){
+            return false;
+        }
+    }
 }
